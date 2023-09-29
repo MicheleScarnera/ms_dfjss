789289d294584a28f1afa711cb335e763cb80929
MicheleScarnera <115538003+MicheleScarnera@users.noreply.github.com>
2023-09-29 12:36:41 +0200

    "Run Autoencoder" file

---
+++ dfjss_run_autoencoder.py
@@ -0,0 +1,21 @@
+import dfjss_nn
+
+dataset = dfjss_nn.IndividualDataset()
+
+for datapoint in dataset:
+    print(f"Original: {dataset.df.loc[0, 'Individual']}")
+    print(f"Reconstructed: {dfjss_nn.string_from_onehots(datapoint)}")
+    #print(datapoint)
+    print(f"Input size: {datapoint.size()}")
+    example = datapoint
+    break
+
+autoencoder = dfjss_nn.IndividualAutoEncoder()
+
+autoencoder.eval()
+autoencoded = autoencoder(example).detach()
+#print(dfjss_nn.string_from_onehots(autoencoded))
+#print(autoencoded)
+print(f"Output size: {autoencoded.size()}")
+
+dfjss_nn.train_autoencoder(autoencoder, dataset, batch_size=16, num_epochs=10)
