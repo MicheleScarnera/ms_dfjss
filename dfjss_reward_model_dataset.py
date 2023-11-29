5adf94e5ebb9fbbfded6dbfdc6d723363bf9d7e1
MicheleScarnera <115538003+MicheleScarnera@users.noreply.github.com>
2023-11-29 16:41:29 +0100

    aaaaaaaaaa

--- dfjss_reward_model_dataset.py
+++ dfjss_reward_model_dataset.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':
     import dfjss_nn
     dfjss_nn.generate_reward_model_file(batch_size=8,
                                         additional_constants_per_batch=4,
-                                        num_batches=4000,
+                                        num_batches=16000,
                                         seeds_per_batch=16,
                                         number_of_possible_seeds=16,
                                         features_weight_in_full_trees=5,
