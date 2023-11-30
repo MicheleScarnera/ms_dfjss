6c4e8387982c545e833b00c1fb17559e8f5bcd1f
MicheleScarnera <115538003+MicheleScarnera@users.noreply.github.com>
2023-11-30 09:25:24 +0100

    i have once again changed my mind about seeds

--- dfjss_reward_model_dataset.py
+++ dfjss_reward_model_dataset.py
@@ -2,8 +2,8 @@ if __name__ == '__main__':
     import dfjss_nn
     dfjss_nn.generate_reward_model_file(batch_size=8,
                                         additional_constants_per_batch=4,
-                                        num_batches=16000,
-                                        seeds_per_batch=16,
-                                        number_of_possible_seeds=16,
+                                        num_batches=100000,
+                                        seeds_per_batch=8,
+                                        number_of_possible_seeds=8,
                                         features_weight_in_full_trees=5,
                                         max_depth=4)
